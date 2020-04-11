2992b20e78bf2724f14ae58d786102ca003af908
lhthang1998 <lhthang.1998@gmail.com>
2020-04-11 14:05:44 +0700

    remove redudant 'http'

--- middlewares/swagger.go
+++ middlewares/swagger.go
@@ -11,7 +11,7 @@ func NewSwagger() gin.HandlerFunc {
 	docs.SwaggerInfo.Title = "UIT Library API"
 	docs.SwaggerInfo.Description = "Swagger for UIT Library API"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "http://libu20.herokuapp.com"
+	docs.SwaggerInfo.Host = "libu20.herokuapp.com"
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	return ginSwagger.WrapHandler(swaggerFiles.Handler)
