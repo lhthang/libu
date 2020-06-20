0197fce745891b3f5a1ce6f8bc17576d4fc8a958
tligodsp <43437080+tligodsp@users.noreply.github.com>
2020-06-20 21:25:23 +0700

    Update Author field in Book

--- app/form/book.go
+++ app/form/book.go
@@ -9,18 +9,20 @@ type BookForm struct {
 	ReleaseAt   string                `form:"releaseAt,omitempty"`
 	Title       string                `form:"title" binding:"required"`
 	CategoryIds []string              `form:"categoryIds,omitempty"`
-	Authors     []string              `form:"authors,omitempty"`
+	AuthorIds   []string              `form:"authorIds,omitempty"`
 	Publisher   string                `form:"publisher,omitempty"`
 	Image       string                `form:"image"  binding:"required"`
 	Description string                `form:"description,omitempty"`
 	Link        string                `form:"link,omitempty"`
 	File        *multipart.FileHeader `form:"file,omitempty"`
+	// Authors     []string              `form:"authors,omitempty"`
 }
 
 type BookResponse struct {
 	*model.Book
 	Reviews    []model.Review   `json:"reviews,omitempty"`
 	Categories []model.Category `json:"categories,omitempty"`
+	Authors    []model.Author   `json:"authors,omitempty"`
 	Rating     float32          `json:"rating"`
 }
 
@@ -28,9 +30,10 @@ type UpdateBookForm struct {
 	ReleaseAt   string   `json:"releaseAt,omitempty"`
 	Title       string   `json:"title" binding:"required"`
 	CategoryIds []string `json:"categoryIds,omitempty"`
-	Authors     []string `json:"authors,omitempty"`
+	AuthorIds   []string `json:"authorIds,omitempty"`
 	Publisher   string   `json:"publisher,omitempty"`
 	Image       string   `json:"image,omitempty"`
 	Description string   `json:"description,omitempty"`
 	Link        string   `json:"link,omitempty"`
+	// Authors     []string `json:"authors,omitempty"`
 }
