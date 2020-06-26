60c9a12e59a192dbf9c360126ad8394b17941acc
tligodsp <43437080+tligodsp@users.noreply.github.com>
2020-06-26 23:45:27 +0700

    Add get reports by review Id

--- app/api/report.go
+++ app/api/report.go
@@ -1,24 +1,49 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"libu/app/form"
 	"libu/app/repository"
 	"libu/middlewares"
 	"libu/my_db"
+	"libu/utils/constant"
 	err2 "libu/utils/err"
 	"libu/utils/jwt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func ApplyReportAPI(app *gin.RouterGroup, resource *my_db.Resource) {
 	reportEntity := repository.NewReportEntity(resource)
 	reportRoute := app.Group("/reports")
+	reviewRoute := app.Group("/reviews")
 
 	reportRoute.Use(middlewares.RequireAuthenticated())
 	reportRoute.POST("", createReport(reportEntity))
+	reviewRoute.Use(middlewares.RequireAuthorization(constant.ADMIN))
+	reviewRoute.GET("/:id/reports", getByReviewId(reportEntity))
 }
 
+// GetByReviewId godoc
+// @Tags ReportController
+// @Summary Get report by review id
+// @Description Get report by review id
+// @Accept  json
+// @Produce  json
+// @Param id path string true "Review ID"
+// @Success 200 {object} model.Report
+// @Router /reviews/{id}/reports [get]
+func getByReviewId(reportEntity repository.IReport) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		reports, code, err := reportEntity.GetByReviewId(id)
+		response := map[string]interface{}{
+			"reports": reports,
+			"error":   err2.GetErrorMessage(err),
+		}
+		ctx.JSON(code, response)
+	}
+}
 
 // CreateReport godoc
 // @Tags ReportController
@@ -40,7 +65,7 @@ func createReport(reportEntity repository.IReport) func(ctx *gin.Context) {
 		}
 		username := jwt.GetUsername(ctx)
 
-		report, code, err := reportEntity.CreateOne(reportForm,username)
+		report, code, err := reportEntity.CreateOne(reportForm, username)
 		response := map[string]interface{}{
 			"report": report,
 			"err":    err2.GetErrorMessage(err),
